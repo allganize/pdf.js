66a41dd329ec338e02d3757b4b978171153e8fe9
Jonas Jenwald <jonas.jenwald@gmail.com>
2020-06-29 13:18:51 +0200

    [api-minor] Use the `NodeCanvasFactory`/`NodeCMapReaderFactory` classes as defaults in Node.js environments (issue 11900)
    This moves, and slightly simplifies, code that's currently residing in the unit-test utils into the actual library, such that it's bundled with `GENERIC`-builds and used in e.g. the API-code.
    
    As an added bonus, this also brings out-of-the-box support for CMaps in e.g. the Node.js examples.

--- gulpfile.js
+++ gulpfile.js
@@ -1611,6 +1611,7 @@ gulp.task(
         bugs: DIST_BUGS_URL,
         license: DIST_LICENSE,
         browser: {
+          canvas: false,
           fs: false,
           http: false,
           https: false,
